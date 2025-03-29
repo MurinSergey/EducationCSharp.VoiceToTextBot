35b1a53082fb60503fecc1b42b070c489919fa7d
Murin Sergey <murin_sergey@vk.com>
2025-03-29 19:15:31 +0300

    Вынес сторонние ресурсы в отдельную папке .speech-detect и добавил архивы вместо исходных файлов.

--- VoiceToTextBot/Utilities/AudioConverter.cs
+++ VoiceToTextBot/Utilities/AudioConverter.cs
@@ -11,7 +11,7 @@ public static class AudioConverter
     public static void TryConvert(string inputFile, string outputFile)
     {
         // Задаём путь, где лежит вспомогательная программа - конвертер
-        GlobalFFOptions.Configure(options => options.BinaryFolder = Path.Combine(DirectoryExtension.GetSolutionRoot(), "ffmpeg-win64", "bin"));
+        GlobalFFOptions.Configure(options => options.BinaryFolder = Path.Combine(DirectoryExtension.GetSolutionRoot(), ".speech-detect", "ffmpeg-win64", "bin"));
 
         // Вызываем Ffmpeg, передав требуемые аргументы.
         FFMpegArguments
--- VoiceToTextBot/Utilities/SpeechDetector.cs
+++ VoiceToTextBot/Utilities/SpeechDetector.cs
@@ -10,7 +10,7 @@ public static class SpeechDetector
     public static string DetectSpeech(string audioPath, float inputBitrate, string languageCode)
     {
         Vosk.Vosk.SetLogLevel(-1); // -1 отключает логи, 0 включает логи
-        var modelPath = Path.Combine(DirectoryExtension.GetSolutionRoot(), "Speech-models", $"vosk-model-small-{languageCode.ToLower()}");
+        var modelPath = Path.Combine(DirectoryExtension.GetSolutionRoot(), ".speech-detect", "Speech-models", $"vosk-model-small-{languageCode.ToLower()}");
         Model model = new(modelPath);
         return GetWords(model, audioPath, inputBitrate);
     }
